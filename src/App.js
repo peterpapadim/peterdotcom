f77108abd007ef7258604a77ea724a7b310a2598
Peter <ppapadim12@gmail.com>
2018-02-11 22:31:07 -0500

    created auto scroller

--- src/App.js
+++ src/App.js
@@ -7,31 +7,97 @@ import ContactMe from './containers/ContactMe';
 import Footer from './containers/Footer';
 import DotNavigator from './containers/DotNavigator';
 import $ from 'jquery';
+var smoothScroll = require('smoothscroll');
 
 class App extends Component {
 
   constructor(){
     super();
     this.state = {
-      inViewDiv: "intro"
+      inViewDiv: "intro",
+      scrollTop: 0
     }
   }
 
+  checkInView = () => {
+      if(this.isScrolledIntoView("#intro-blurb") && this.state.inViewDiv !== "intro"){
+        this.setState({inViewDiv: "intro"})
+      }
+      if(this.isScrolledIntoView("#bio-container") && this.state.inViewDiv !== "about"){
+        this.setState({inViewDiv: "about"})
+      }
+      if(this.isScrolledIntoView("#tech-skills-container") && this.state.inViewDiv !== "skills"){
+        this.setState({inViewDiv: "skills"})
+      }
+      if(this.isScrolledIntoView("#reelsquad-text") && this.state.inViewDiv !== "projects"){
+        this.setState({inViewDiv: "projects"})
+      }
+  }
+
+  autoScroll = () => {
+    if(this.state.inViewDiv === "intro" && $(window).scrollTop() > this.state.scrollTop){
+      window.removeEventListener('scroll', this.autoScroll)
+      let element = document.getElementById("about-container")
+      smoothScroll(element, 1000, () => {
+        this.setState({scrollTop: $(window).scrollTop()})
+        window.addEventListener('scroll', this.autoScroll);
+      })
+    }
+    if(this.state.inViewDiv === "about" && $(window).scrollTop() < this.state.scrollTop){
+      window.removeEventListener('scroll', this.autoScroll)
+      let element = document.getElementById("intro-container")
+      smoothScroll(element, 1000, () => {
+        this.setState({scrollTop: $(window).scrollTop()})
+        window.addEventListener('scroll', this.autoScroll);
+      })
+    }
+    if(this.state.inViewDiv === "about" && $(window).scrollTop() > this.state.scrollTop){
+      window.removeEventListener('scroll', this.autoScroll)
+      let element = document.getElementById("skills-container")
+      smoothScroll(element, 1000, () => {
+        this.setState({scrollTop: $(window).scrollTop()})
+        window.addEventListener('scroll', this.autoScroll);
+      })
+    }
+    if(this.state.inViewDiv === "skills" && $(window).scrollTop() < this.state.scrollTop){
+      window.removeEventListener('scroll', this.autoScroll)
+      let element = document.getElementById("about-container")
+      smoothScroll(element, 1000, () => {
+        this.setState({scrollTop: $(window).scrollTop()})
+        window.addEventListener('scroll', this.autoScroll);
+      })
+    }
+    if(this.state.inViewDiv === "skills" && $(window).scrollTop() > this.state.scrollTop){
+      window.removeEventListener('scroll', this.autoScroll)
+      let element = document.getElementById("projects-container-1")
+      smoothScroll(element, 1000, () => {
+        this.setState({scrollTop: $(window).scrollTop()})
+        window.addEventListener('scroll', this.autoScroll);
+      })
+    }
+    if(this.state.inViewDiv === "projects" && $(window).scrollTop() < this.state.scrollTop){
+      window.removeEventListener('scroll', this.autoScroll)
+      let element = document.getElementById("skills-container")
+      smoothScroll(element, 1000, () => {
+        this.setState({scrollTop: $(window).scrollTop()})
+        window.addEventListener('scroll', this.autoScroll);
+      })
+    }
+  }
+
+  handleDotClick = (id) => {
+    window.removeEventListener('scroll', this.autoScroll)
+    let element = document.getElementById(id)
+    smoothScroll(element, 1000, () => {
+      this.setState({scrollTop: $(window).scrollTop()})
+      window.addEventListener('scroll', this.autoScroll);
+    })
+  }
+
   componentDidMount() {
-   window.addEventListener('scroll', (event) => {
-     if(this.isScrolledIntoView("#intro-blurb") && this.state.inViewDiv !== "intro"){
-       this.setState({inViewDiv: "intro"})
-     }
-     if(this.isScrolledIntoView("#bio-container") && this.state.inViewDiv !== "about"){
-       this.setState({inViewDiv: "about"})
-     }
-     if(this.isScrolledIntoView("#tech-skills-container") && this.state.inViewDiv !== "skills"){
-       this.setState({inViewDiv: "skills"})
-     }
-     if(this.isScrolledIntoView("#reelsquad-text") && this.state.inViewDiv !== "projects"){
-       this.setState({inViewDiv: "projects"})
-     }
-   });
+   this.setState({scrollTop: $(window).scrollTop()})
+   window.addEventListener('scroll', this.checkInView);
+   window.addEventListener('scroll', this.autoScroll);
   }
 
   isScrolledIntoView = (elem) => {
@@ -47,7 +113,7 @@ class App extends Component {
   render() {
     return (
       <div id="App" className="App">
-        <DotNavigator inViewDiv={this.state.inViewDiv}/>
+        <DotNavigator inViewDiv={this.state.inViewDiv} handleDotClick={this.handleDotClick}/>
         <Intro />
         <About />
         <Skills />
