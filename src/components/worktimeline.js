55e73aa96baff95344ea32d570ff6e7e555d146d
Peter <ppapadim12@gmail.com>
2018-08-16 22:04:20 -0400

    Homepage 'worktimeline' alignment complete. Component aligned with remainder of page.

--- src/components/worktimeline.js
+++ src/components/worktimeline.js
@@ -5,13 +5,50 @@ import '../assets/styling/worktimeline.css';
 class WorkTimeline extends Component {
   constructor(){
     super();
-    this.state = {}
+    this.state = {
+      styles: {
+        paddingRight: 0,
+        paddingLeft: 0
+      }
+    }
   }
+
+  componentDidMount(){
+    this.setPaddingLeftAndRight()
+    window.addEventListener('resize', this.setPaddingLeftAndRight)
+  }
+
+  componentWillUnmount(){
+    window.removeEventListener('resize', this.setPaddingLeftAndRight);
+  }
+
+  setPaddingLeftAndRight = () => {
+    let windowWidth = window.innerWidth
+    let elementWidth = document.getElementsByClassName('blurb')[0].clientWidth
+    let elementLeft = document.getElementsByClassName('blurb')[0].offsetLeft
+    let paddingRight = (windowWidth) - (elementWidth + elementLeft) + (elementWidth - 290)
+
+    // Adding 30 because offsetLeft does not include element padding (just margin).
+    // Subract 40 to offset the width of a timeline square.
+
+    let paddingLeft = document.getElementsByClassName('blurb')[0].offsetLeft + 30 - 40
+
+    this.setState({
+        styles: Object.assign(
+          {},
+          this.state.styles,
+          {
+            paddingRight: paddingRight,
+            paddingLeft: paddingLeft
+          }
+        )
+    })
+  }
+
 
   render(){
     return(
-        <div className='worktimeline-container'>
-          <WorkTimelineItem />
+        <div className='worktimeline-container' style={this.state.styles}>
           <WorkTimelineItem />
           <WorkTimelineItem />
           <WorkTimelineItem />
